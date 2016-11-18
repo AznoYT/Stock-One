3835ab6b181fa98356d93bd907ad6b8779a2dd26
Azno <ugo31140@gmail.com>
2016-11-18 23:47:04 +0100

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -3,15 +3,24 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Stock One - Identification</title>
-		<link rel="stylesheet" type="text/css" href="./css/style.css" />
-		<script language="javascript" type="text/javascript" src="./js/script.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/style.css" />
+		<script language="javascript" type="text/javascript" src="../js/script.js"></script>
 	</head>
 	<body>
 		<header>
 			<h1>Stock One </h1>
 		</header>
 		<section>
-			
+			<form method="post" action="veriflogin.php">
+				<fieldset>
+					<legend>Connexion:</legend>
+					Nom d'Utilisateur:<br>
+					<input type="text" name="utilisateur" value=""><br>
+					Mot de passe:<br>
+					<input type="password" name="pws" value=""><br><br>
+					<input type="submit" value="Connexion">
+				</fieldset>
+			</form>
 		</section>
 		<footer>
 			<h4></h4>
