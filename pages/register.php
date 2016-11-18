c7f8dc62e1482db40ab5d304b26fa462d2231cf5
Azno <ugo31140@gmail.com>
2016-11-18 16:58:16 +0100

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -11,7 +11,21 @@
 			<h1>Stock One </h1>
 		</header>
 		<section>
-			
+			<form method="get" action="validate.php">
+				<fieldset>
+					<legend>Information Personnel:</legend>
+					Nom:<br>
+					<input type="text" name="Nom" value=""><br>
+					Prénom:<br>
+					<input type="text" name="prenom" value=""><br><br>
+					<input type="radio" name="genre" value="Homme" checked> Homme<br>
+					<input type="radio" name="genre" value="Femme"> Femme<br>
+					<input type="radio" name="genre" value="Autres"> Autres<br><br>
+					E-mail:<br>
+					<input type="email" name="email"><br><br>
+					<input type="submit" value="Envoyer"><input type="reset" value="Tout Effacer">
+				</fieldset>
+			</form>
 		</section>
 		<footer>
 			<h4></h4>
