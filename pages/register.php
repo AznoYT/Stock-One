c72d33a91c0d048465da81d0bb648c7616c8d5b0
Azno <ugo31140@gmail.com>
2016-11-18 17:09:22 +0100

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -14,15 +14,20 @@
 			<form method="get" action="validate.php">
 				<fieldset>
 					<legend>Information Personnel:</legend>
+					Nom d'utilisateur:<br>
+					<input type="text" name="utilisateur" value=""><br>
 					Nom:<br>
 					<input type="text" name="Nom" value=""><br>
 					Prénom:<br>
 					<input type="text" name="prenom" value=""><br><br>
+					Sexe:<br>
 					<input type="radio" name="genre" value="Homme" checked> Homme<br>
 					<input type="radio" name="genre" value="Femme"> Femme<br>
 					<input type="radio" name="genre" value="Autres"> Autres<br><br>
 					E-mail:<br>
 					<input type="email" name="email"><br><br>
+					<input type="checkbox" name="notif" value="notif"> Souhaitez vous recevoir des notifications de la part de Stock-One<br>
+					<input type="checkbox" name="notifpart" value="notifpart"> Souhaitez vous que les Partenaires de Stock-One puisse vous Contacter<br><br>
 					<input type="submit" value="Envoyer"><input type="reset" value="Tout Effacer">
 				</fieldset>
 			</form>
