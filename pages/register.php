dbd73d1935f2e15ff8768a012876616d5f1b6d39
Azno <ugo31140@gmail.com>
2016-11-18 16:43:56 +0100

    Add files via upload

---
+++ pages/register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Stock One - Inscription</title>
+		<link rel="stylesheet" type="text/css" href="./css/style.css" />
+		<script language="javascript" type="text/javascript" src="./js/script.js"></script>
+	</head>
+	<body>
+		<header>
+			<h1>Stock One </h1>
+		</header>
+		<section>
+			
+		</section>
+		<footer>
+			<h4></h4>
+		</footer>
+	</body>
+</html>
