78e3124a2708ecc683c7800a45e148633d86802c
Azno <ugo31140@gmail.com>
2016-11-18 23:46:06 +0100

    Add files via upload

---
+++ pages/upload.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Stock One - Inscription</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css" />
+		<script language="javascript" type="text/javascript" src="../js/script.js"></script>
+	</head>
+	<body>
+		<header>
+			<h1>Stock One </h1>
+		</header>
+		<section>
+			<form method="post" action="vupload.php">
+				<fieldset>
+					<legend>Envoyer le fichier :</legend><br><br>
+						<input name="fichier" type="file" /><br><br>
+						<input type="submit" name="submit" value="Uploader" />
+				</fieldset>
+			</form>
+		</section>
+		<footer>
+			<h4></h4>
+		</footer>
+	</body>
+</html>
---
+++ pages/validate.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Stock One - Validation</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css" />
+		<script language="javascript" type="text/javascript" src="../js/script.js"></script>
+	</head>
+	<body>
+		<header>
+			<h1>Stock One </h1>
+		</header>
+		<section>
+			<?php
+				echo $_POST['utilisateur'];
+				echo $_POST['Nom'];
+			?>
+		</section>
+		<footer>
+			<h4></h4>
+		</footer>
+	</body>
+</html>
