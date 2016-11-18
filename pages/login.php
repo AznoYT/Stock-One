b14a0b75bff5125317fbceb6f54e2ae97a003213
Tracks <anon.dark97@gmail.com>
2016-11-18 16:18:41 +0100

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -1,9 +1,20 @@
 <!DOCTYPE html>
 <html>
-  <head>
+	<head>
 		<meta charset="UTF-8">
 		<title>Stock One - Cloud</title>
 		<link rel="stylesheet" type="text/css" href="./css/style.css" />
 		<script language="javascript" type="text/javascript" src="./js/script.js"></script>
 	</head>
+	<body>
+		<header>
+			<h1>Stock One </h1>
+		</header>
+		<section>
+			
+		</section>
+		<footer>
+			<h4></h4>
+		</footer>
+	</body>
 </html>
