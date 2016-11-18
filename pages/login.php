b700cd307d3983c0f19b7053be7e777b57b3ad33
Azno <ugo31140@gmail.com>
2016-11-18 16:07:40 +0100

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,9 @@
 <!DOCTYPE html>
 <html>
+  <head>
+		<meta charset="UTF-8">
+		<title>Stock One - Cloud</title>
+		<link rel="stylesheet" type="text/css" href="./css/style.css" />
+		<script language="javascript" type="text/javascript" src="./js/script.js"></script>
+	</head>
 </html>
