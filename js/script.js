dc7494829f57a24d1a456e9c0beff64c00dd6313
Tracks <anon.dark97@gmail.com>
2016-11-18 17:22:26 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@
 ************************/
 
 function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML = h+ ":" + m + ":" + s;
-    t = setTimeout(function(){ startTime() }, 500);
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('txt').innerHTML = h+ ":" + m + ":" + s;
+	t = setTimeout(function(){ startTime() }, 500);
 }
 
 function checkTime(i) {
-    if (i<10) {
-        i = "0" + i;
-    }
-    return i;
+	if (i<10) {
+		i = "0" + i;
+	}
+	return i;
 }
 
 /******
