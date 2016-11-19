dbafeef3142c83c2f871813dcafc06e916e2873c
Azno <ugo31140@gmail.com>
2016-11-19 17:05:32 +0100

    Mise à jour du validate.php
    Ajouts des Retours Informations, et debut de la connection Mysql

--- pages/validate.php
+++ pages/validate.php
@@ -12,8 +12,49 @@
 		</header>
 		<section>
 			<?php
+				echo 'Utilisateur :';
 				echo $_POST['utilisateur'];
+				echo "</br>";
+				echo 'Nom :';
 				echo $_POST['Nom'];
+				echo "</br>";
+				echo 'Prénom :';
+				echo $_POST['prenom'];
+				echo "</br>";
+				echo 'Sexe :';
+				echo $_POST['genre'];
+				echo "</br>";
+				echo 'Email:';
+				echo $_POST['email'];
+				echo "</br>";
+				echo 'Notif Stock-One:';
+				echo $_POST['notif'];
+				echo "</br>";
+				echo 'Notif Partenaire:';
+				echo $_POST['notifpart'];
+				
+				$servername = "51.255.34.189";
+				$username = "root";
+				$password = "ugo31140";
+				$dbname = "Stock-One";
+
+				// Create connection
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				// Check connection
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				} 
+
+				$sql = "INSERT INTO user (utilisateur, nom, prenom, genre, email, Notif Stock-One, Notif Partenaire)
+				VALUES ('utilisateur', 'Nom', 'prenom', 'genre', 'email', 'notif', 'notifpart')";
+
+				if ($conn->query($sql) === TRUE) {
+					echo "New record created successfully";
+				} else {
+					echo "Error: " . $sql . "<br>" . $conn->error;
+				}
+
+				$conn->close();
 			?>
 		</section>
 		<footer>
