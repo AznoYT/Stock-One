a82ed9bb5270f2ffb140e98f7e83448367c73f6d
Tracks <anon.dark97@gmail.com>
2016-12-17 15:46:52 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -24,17 +24,25 @@ function checkTime(i) {
 
 function verify(pws1, pws2) {
 	var passed = false;
+	var msg1 = document.getElementById('msg1');
+	var msg2 = document.getElementById('msg2');
 	
 	if(pws1.value == '') {
-		alert("Veuillez entrer votre mot de passe dans le premier champs.");
+		//alert("Veuillez entrer votre mot de passe dans le premier champs.");
+		msg1.innerHTML = ' <-- Ce champs est vide !';
+		msg2.innerHTML = '';
 		pws1.focus();
 	}
 	else if(pws2.value == '') {
-		alert("Veuillez entrer votre mot de passe dans le second champs.");
+		//alert("Veuillez entrer votre mot de passe dans le second champs.");
+		msg1.innerHTML = '';
+		msg2.innerHTML = ' <-- Ce champs est vide !';
 		pws2.focus();
 	}
 	else if(pws1.value != pws2.value) {
-		alert("Les 2 mot de passe ne correspondent pas.");
+		//alert("Les 2 mot de passe ne correspondent pas.");
+		msg1.innerHTML = ' <-- Les 2 champs sont différents !';
+		msg2.innerHTML = ' <-- Les 2 champs sont différents !';
 		pws1.select();
 	}
 	else {
@@ -53,7 +61,7 @@ function popuplogin(login) {
 	}
 	if(login == 2) {
 		// Ici le Register
-		popup.innerHTML = '<form method="post" action="./pages/validate.php" onsubmit="return verify(this.pws, this.pws1);"><fieldset><legend>Information Personnel:</legend><label>Nom d\'utilisateur:</label><br><input type="text" name="utilisateur" value="" /><br><label>Nom:</label><br><input type="text" name="Nom" value="" /><br><label>Prénom:</label><br><input type="text" name="prenom" value="" /><br><br><label>Sexe:</label><br><input type="radio" name="genre" value="Homme" checked /> Homme<br><input type="radio" name="genre" value="Femme" /> Femme<br><input type="radio" name="genre" value="Autres" /> Autres<br><br><label>E-mail:</label><br><input type="email" name="email" /><br/><br/><label>Mot de passe:</label><br/><input type="password" name="pws" value="" /><br><label>Confirmer votre Mot de Passe:</label><br><input type="password" name="pws1" value="" /><br><br><input type="checkbox" name="notif" value="1" /> Souhaitez vous recevoir des notifications de la part de Stock-One<br><input type="checkbox" name="notifpart" value="1" /> Souhaitez vous que les Partenaires de Stock-One puisse vous Contacter<br><br><input type="submit" value="Envoyer" /><input type="reset" value="Tout Effacer" /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset></form>';
+		popup.innerHTML = '<form method="post" action="./pages/validate.php" onsubmit="return verify(this.pws, this.pws1);"><fieldset><legend>Inscription:</legend><label>Nom d\'utilisateur:</label><br><input type="text" name="utilisateur" value="" /><br><label>Nom:</label><br><input type="text" name="Nom" value="" /><br><label>Prénom:</label><br><input type="text" name="prenom" value="" /><br><br><label>Sexe:</label><br><input type="radio" name="genre" value="Homme" checked /> Homme<br><input type="radio" name="genre" value="Femme" /> Femme<br><input type="radio" name="genre" value="Autres" /> Autres<br><br><label>E-mail:</label><br><input type="email" name="email" /><br/><br/><label>Mot de passe:</label><br/><input type="password" name="pws" value="" /><font style="color: #FF0000;" id="msg1"></font><br><label>Confirmer votre Mot de Passe:</label><br><input type="password" name="pws1" value="" /><font style="color: #FF0000;" id="msg2"></font><br><br><input type="checkbox" name="notif" value="1" /> Souhaitez vous recevoir des notifications de la part de Stock-One<br><input type="checkbox" name="notifpart" value="1" /> Souhaitez vous que les Partenaires de Stock-One puisse vous Contacter<br><br><input type="submit" value="Envoyer" /><input type="reset" value="Tout Effacer" /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset></form>';
 	}
 }
 
@@ -66,15 +74,15 @@ function popupaction(action) {
 	}
 	if(action == 1) {
 		// Ici l'upload
-		popup.innerHTML = '<form method="post" action="./pages/vupload.php"><fieldset><legend>Envoyer un fichier :</legend><br><input name="file" type="file" /><br><br><input type="checkbox" name="Public" value="notif"><label>Publique</label><br/><br/><input type="submit" name="submit" value="Importer" /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset></form';
+		popup.innerHTML = '<form method="post" action="./pages/vupload.php"><fieldset><legend>Envoyer un fichier:</legend><br><input name="file" type="file" /><br><br><input type="checkbox" name="Public" value="notif"><label>Publique</label><br/><br/><input type="submit" name="submit" value="Importer" /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset></form';
 	}
 	if(action == 2) {
 		// Ici le download
-		popup.innerHTML = '<fieldset><legend>Recevoir un fichier :</legend><p>En Construction</p><br /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset>';
+		popup.innerHTML = '<fieldset><legend>Recevoir un fichier:</legend><p>En Construction</p><br /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset>';
 	}
 	if(action == 3) {
 		// Ici la deconnexion
-		popup.innerHTML = '<fieldset><legend>Déconnexion :</legend><p>En Construction</p><br /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset>';
+		popup.innerHTML = '<fieldset><legend>Déconnexion:</legend><p>En Construction</p><br /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset>';
 	}
 }
 
