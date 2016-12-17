2ce4fc763558e6ebb3c37231891901090bdd69e5
Tracks <anon.dark97@gmail.com>
2016-12-17 23:42:18 +0100

    Update disconnect.php

--- pages/disconnect.php
+++ pages/disconnect.php
@@ -4,16 +4,16 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Stock One - Déconnexion</title>
+		<title>Stock One - DÃ©connexion</title>
 		<link rel="stylesheet" type="text/css" href="../css/style.css" />
 		<script language="javascript" type="text/javascript" src="../js/script.js"></script>
 	</head>
 	<?php
 		// mieux de le faire avec un try car la connexion sera permanante
 		try {
-			$bdd = new PDO('mysql:host=127.0.0.1;dbname=stock-one;charset=utf8', 'root', 'toor');
+			$bdd = new PDO('mysql:host=127.0.0.1;dbname=stock-one;charset=utf8', 'root', '');
 		}
-		catch(Exception $e) { // au cas-où si ça foire il affiche la couille dans le paté
+		catch(Exception $e) { // au cas-oÃ¹ si Ã§a foire il affiche la couille dans le patÃ©
 			die('ERROR : '.$e->getMessage());
 		}
 	?>
