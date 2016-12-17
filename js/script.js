e3c19c1ac430590750762aca89d08c4d702398f3
Tracks <anon.dark97@gmail.com>
2016-12-17 15:16:56 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -49,11 +49,11 @@ function popuplogin(login) {
 	
 	if(login == 1) {
 		// Ici le Login
-		popup.innerHTML = '<form method="post" action="./pages/veriflogin.php"><fieldset><legend>Connexion:</legend><label>Nom d\'Utilisateur:</label><br><input type="text" name="lutilisateur" ><br><label>Mot de passe:</label><br><input type="password" name="lpws" ><br><br><input type="submit" value="Connexion"></fieldset></form>';
+		popup.innerHTML = '<form method="post" action="./pages/veriflogin.php"><fieldset><legend>Connexion:</legend><label>Nom d\'Utilisateur:</label><br><input type="text" name="lutilisateur" ><br><label>Mot de passe:</label><br><input type="password" name="lpws" ><br><br><input type="submit" value="Connexion"><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset></form>';
 	}
 	if(login == 2) {
 		// Ici le Register
-		popup.innerHTML = '<form method="post" action="./pages/validate.php" onsubmit="return verify(this.pws, this.pws1);"><fieldset><legend>Information Personnel:</legend><label>Nom d\'utilisateur:</label><br><input type="text" name="utilisateur" value="" /><br><label>Nom:</label><br><input type="text" name="Nom" value="" /><br><label>Prénom:</label><br><input type="text" name="prenom" value="" /><br><br><label>Sexe:</label><br><input type="radio" name="genre" value="Homme" checked /> Homme<br><input type="radio" name="genre" value="Femme" /> Femme<br><input type="radio" name="genre" value="Autres" /> Autres<br><br><label>E-mail:</label><br><input type="email" name="email" /><br/><br/><label>Mot de passe:</label><br/><input type="password" name="pws" value="" /><br><label>Confirmer votre Mot de Passe:</label><br><input type="password" name="pws1" value="" /><br><br><input type="checkbox" name="notif" value="1" /> Souhaitez vous recevoir des notifications de la part de Stock-One<br><input type="checkbox" name="notifpart" value="1" /> Souhaitez vous que les Partenaires de Stock-One puisse vous Contacter<br><br><input type="submit" value="Envoyer" /><input type="reset" value="Tout Effacer" /></fieldset></form>';
+		popup.innerHTML = '<form method="post" action="./pages/validate.php" onsubmit="return verify(this.pws, this.pws1);"><fieldset><legend>Information Personnel:</legend><label>Nom d\'utilisateur:</label><br><input type="text" name="utilisateur" value="" /><br><label>Nom:</label><br><input type="text" name="Nom" value="" /><br><label>Prénom:</label><br><input type="text" name="prenom" value="" /><br><br><label>Sexe:</label><br><input type="radio" name="genre" value="Homme" checked /> Homme<br><input type="radio" name="genre" value="Femme" /> Femme<br><input type="radio" name="genre" value="Autres" /> Autres<br><br><label>E-mail:</label><br><input type="email" name="email" /><br/><br/><label>Mot de passe:</label><br/><input type="password" name="pws" value="" /><br><label>Confirmer votre Mot de Passe:</label><br><input type="password" name="pws1" value="" /><br><br><input type="checkbox" name="notif" value="1" /> Souhaitez vous recevoir des notifications de la part de Stock-One<br><input type="checkbox" name="notifpart" value="1" /> Souhaitez vous que les Partenaires de Stock-One puisse vous Contacter<br><br><input type="submit" value="Envoyer" /><input type="reset" value="Tout Effacer" /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset></form>';
 	}
 }
 
@@ -61,9 +61,12 @@ function popuplogin(login) {
 function popupaction(action) {
 	var popup = document.getElementById('popup');
 	
+	if(action == 0) {
+		popup.innerHTML = '';
+	}
 	if(action == 1) {
 		// Ici l'upload
-		popup.innerHTML = '<form method="post" action="./pages/vupload.php"><fieldset><legend>Envoyer le fichier :</legend><br><input name="file" type="file" /><br><br><input type="checkbox" name="Public" value="notif"><label>Publique</label><br/><br/><input type="submit" name="submit" value="Uploader" /></fieldset></form';
+		popup.innerHTML = '<form method="post" action="./pages/vupload.php"><fieldset><legend>Envoyer le fichier :</legend><br><input name="file" type="file" /><br><br><input type="checkbox" name="Public" value="notif"><label>Publique</label><br/><br/><input type="submit" name="submit" value="Importer" /><input type="button" onclick="popupaction(0);" value="Annuler" /></fieldset></form';
 	}
 	if(action == 2) {
 		// Ici le download
