e64b4ffca5ea87a514f76440649eb93815be03be
Tracks <anon.dark97@gmail.com>
2017-01-07 20:52:08 +0100

    Update validate.php

--- pages/validate.php
+++ pages/validate.php
@@ -11,7 +11,7 @@
 	</head>
 	<?php
 		try {
-			$bdd = new PDO('mysql:host=127.0.0.1;dbname=stock-one;charset=utf8', 'root', 'toor', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+			$bdd = new PDO('mysql:host=127.0.0.1;dbname=stock-one;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 		}
 		catch(Exception $e) {
 			die('ERROR : '.$e->getMessage());
