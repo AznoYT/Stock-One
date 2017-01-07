8a715498ea1155709aefa72c62ca48673c91e6e1
Tracks <anon.dark97@gmail.com>
2017-01-07 20:51:40 +0100

    Update disconnect.php

--- pages/disconnect.php
+++ pages/disconnect.php
@@ -4,14 +4,14 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Stock One - Déconnexion</title>
+		<title>Stock One - DÃ©connexion</title>
 		<link rel="stylesheet" type="text/css" href="../css/style.css" />
 		<link rel="icon" type="image/png" href="../pics/icon.png" />
 		<script language="javascript" type="text/javascript" src="../js/script.js"></script>
 	</head>
 	<?php
 		try {
-			$bdd = new PDO('mysql:host=127.0.0.1;dbname=stock-one;charset=utf8', 'root', 'toor');
+			$bdd = new PDO('mysql:host=127.0.0.1;dbname=stock-one;charset=utf8', 'root', '');
 		}
 		catch(Exception $e) {
 			die('ERROR : '.$e->getMessage());
