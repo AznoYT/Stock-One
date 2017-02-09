d515f3239d6f649e9fd0b74548b72caf85f39ed2
Tracks <anon.dark97@gmail.com>
2017-02-09 17:55:09 +0100

    Update bdd_access.php

--- bdd_access.php
+++ bdd_access.php
@@ -1,7 +1,13 @@
 <?php
 	// bdd_access.php
+	$host = '127.0.0.1';
+	$db_name = 'stock-one';
+	$encoding = 'utf8';
+	$username = 'root';
+	$password = 'toor';
+	
 	session_start();
-	try { $bdd = new PDO('mysql:host=127.0.0.1;dbname=stock-one;charset=utf8', 'root', 'toor', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)); }
+	try { $bdd = new PDO('mysql:host='.$host.';dbname='.$db_name.';charset='.$encoding, $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)); }
 	catch(Exception $e) { die('ERROR : '.$e->getMessage()); }
 	// END
 ?>
