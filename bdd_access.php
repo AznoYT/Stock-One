66d3a1b6aa626216dbc6e3f55dd562a779dd9012
Tracks <anon.dark97@gmail.com>
2017-02-09 17:44:38 +0100

    Add files via upload

--- bdd_access.php
+++ bdd_access.php
@@ -1,5 +1,7 @@
+<!-- bdd_access.php -->
 <?php
 	session_start();
 	try { $bdd = new PDO('mysql:host=127.0.0.1;dbname=stock-one;charset=utf8', 'root', 'toor', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)); }
 	catch(Exception $e) { die('ERROR : '.$e->getMessage()); }
 ?>
+<!-- END -->
