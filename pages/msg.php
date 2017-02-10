100ae373461bccec40d0178d0b89cf0983b02de9
Tracks <anon.dark97@gmail.com>
2017-02-10 23:53:37 +0100

    Update msg.php

--- pages/msg.php
+++ pages/msg.php
@@ -1,4 +1,5 @@
 <?php
+	// Fichier d'affichage de message de confirmation de commandes executé
 	switch($_GET['etat']) {
 		case 'OK': $msg = '<font id="msg3">> L\'action mené au '.$objet.' à bien été éxecuter.</font>'; break;
 		case 'ERREUR': $msg = '<font id="msg0">> L\'action mené au '.$objet.' fichier à rencontrer une erreur.</font>'; break;
