a073aaba879576dac47e003c17be7beede946eb7
Tracks <anon.dark97@gmail.com>
2017-02-10 19:42:39 +0100

    Add files via upload

---
+++ pages/msg.php
@@ -0,0 +1,13 @@
+<?php
+	switch($_GET['etat']) {
+		case 'OK': $msg = '<font id="msg3">> L\'action mené au '.$objet.' à bien été éxecuter.</font>'; break;
+		case 'ERREUR': $msg = '<font id="msg0">> L\'action mené au '.$objet.' fichier à rencontrer une erreur.</font>'; break;
+	}
+	
+	echo("<fieldset>");
+	echo("<Legend>Confirmation de $action:</Legend>");
+	echo($msg);
+	echo("<br /><br />");
+	echo('<input type="button" onclick="moreaction(0); document.location = \'./'.$directory.'\';" value="Fermer" />');
+	echo("</fieldset>");
+?>
