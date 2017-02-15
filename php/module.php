916e3847ecbeb9c30c2e591cee2b3bab09712947
Tracks <anon.dark97@gmail.com>
2017-02-15 18:26:50 +0100

    Add files via upload

--- php/module.php
+++ php/module.php
@@ -15,5 +15,30 @@
 		else { echo(''); }
 	}
 	
+	function action($COMMAND, $db, $propriétaire, $nom, $dir, $dirpaste) {
+		// Dans la fonction il y aura le mode d'action de donnée dans la database. Elle sera ensuite appeler par chaque conditions
+		// ICI sera insérer le code PHP executant l'action sur le fichier choisi
+		
+		
+		// Cette Partie du code concerne le référencement dans la base donnée
+		$data = $db->query('SELECT * FROM donnee');
+		
+		while($file = $data->fetch()) {
+			if($propriétaire == $file[1]) {
+				if($nom == $file[3]) {
+					switch($COMMAND) {
+						case 0: $stmt = $db->prepare('INSERT INTO donnee(identifiant, type, nom, nom_dossier, taille, public) VALUES ("'.$propriétaire.'", "'.$file[2].'", "'.$nom.'", "'.$file[4].'", "'.$file[5].'", "'.$file[7].'")'); break; // La copie
+						case 1: $stmt = $db->prepare('UPDATE donnee SET nom_dossier'); break; // Le déplacement
+						case 2: $stmt = $db->prepare('DELETE FROM donnee WHERE nom="'.$nom.'"'); break; // La suppression
+					}
+					
+					$stmt->execute();
+				}
+			}
+		}
+		
+		return 'OK';
+	}
+	
 	// END
 ?>
