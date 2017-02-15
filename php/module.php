0375f1f32a0395b4ec84a929fcb648b5fc64a17d
Tracks <anon.dark97@gmail.com>
2017-02-15 18:28:41 +0100

    Add files via upload

--- php/module.php
+++ php/module.php
@@ -1,7 +1,9 @@
 <?php
 	// module.php
 	// Fichier de fonction php
-	function list_fichiers($file, $dir, $a) { // Fonction de listing de fichiers
+	
+	// Fonction de listing de fichiers
+	function list_fichiers($file, $dir, $a) {
 		if($file[2] == 'folder') { echo(''); }
 		else if($file[2] == 'png' || $file[2] == 'jpeg' || $file[2] == 'jpg' || $file[2] == 'gif' || $file[2] == 'bmp' ) { echo("<img class=\"classement\" height=\"15px\" src=\"$a./pics/".$dir."gallery.png\" /><input class=\"list\" type=\"button\" onclick=\"popupaction(4, '$file[4]$file[3]', 1, '$file[3]', '$file[5]', '$file[7]');\" value=\"$file[3]\" title=\"$file[3]\" /><br />"); }
 		else if($file[2] == 'mp3' || $file[2] == 'wav' || $file[2] == 'wma' || $file[2] == 'aac' || $file[2] == 'ac3' || $file[2] == 'm4a') { echo("<img class=\"classement\" height=\"15px\" src=\"$a./pics/".$dir."music.png\" /><input class=\"list\" type=\"button\" onclick=\"popupaction(4, '$file[4]$file[3]', 2, '$file[3]', '$file[5]', '$file[7]');\" value=\"$file[3]\" title=\"$file[3]\" /><br />"); }
@@ -10,12 +12,14 @@
 		else { echo("<img class=\"classement\" height=\"15px\" src=\"$a./pics/".$dir."text-file.png\" /><a href=\"$a$file[4]$file[3]\" download><input class=\"list\" type=\"button\" value=\"$file[3]\" title=\"$file[3]\" /></a><br />"); }
 	}
 	
-	function list_dossiers($file, $dir, $a) { // Fonction de listing de dossiers
+	// Fonction de listing de dossiers
+	function list_dossiers($file, $dir, $a) {
 		if($file[2] == 'folder') { echo("<img class=\"classement\" height=\"15px\" src=\"$a./pics/".$dir."folder.png\" /><input class=\"list\" type=\"submit\" name=\"folder\" value=\"$file[3]\" title=\"$file[3]\" /><br />"); }
 		else { echo(''); }
 	}
 	
-	function action($COMMAND, $db, $propriétaire, $nom, $dir, $dirpaste) { // Fonction de commande Copie/Déplacement/Suppression de fichiers
+	// Fonction de commande Copie/Déplacement/Suppression de fichiers
+	function action($COMMAND, $db, $propriétaire, $nom, $dir, $dirpaste) {
 		// Dans la fonction il y aura le mode d'action de donnée dans la database. Elle sera ensuite appeler par chaque conditions
 		// ICI sera insérer le code PHP executant l'action sur le fichier choisi
 		
