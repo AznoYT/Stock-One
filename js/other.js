9d0aa73738b934181a2ce7011731f67570a9742b
Tracks <anon.dark97@gmail.com>
2017-03-04 01:39:11 +0100

    Add files via upload

--- js/other.js
+++ js/other.js
@@ -8,9 +8,9 @@ window.addEventListener('load', beg_func, false);
 var COLS=50, ROWS= 17, SPACE=0, SNAKE=1, FOOD=2, LEFT=0, UP=1, RIGHT=2, DOWN=3, KEY_LEFT=37, KEY_UP=38, KEY_RIGHT=39, KEY_DOWN=40, KEY_SPC=32, KEY_ESC=27, canvas, context, keystate, frames, score, fr=0;
 
 grid = {
-	width: null, 
+	width: null,
 	height: null,
-	grids: null,  
+	grids: null,
 	init: function(d, c, r){
 		this.width = c;
 		this.height = r;
@@ -36,7 +36,7 @@ snake = {
 		this.insert(x, y);
 	},
 	insert:function(x, y) {
-		this.que.unshift({x:x, y:y});
+		this.que.unshift({ x:x, y:y });
 		this.last = this.que[0];
 	},
 	remove:function() { return this.que.pop(); }
@@ -47,7 +47,7 @@ function setBlob() {
 	
 	for(var x = 0;x < grid.width; x++) {
 		for(var y = 0;y < grid.height; y++) {
-			if(grid.get(x,y) == SPACE) { space.push({x:x, y:y}); }
+			if(grid.get(x,y) == SPACE) { space.push({ x:x, y:y }); }
 		}
 	}
 	
@@ -80,7 +80,7 @@ function beg_func() {
 function init() {
 	score = 0;fr = 0;
 	grid.init(SPACE, COLS, ROWS);
-	var snake_pos={x:0, y:0};
+	var snake_pos = { x:0, y:0 };
 	snake.init(LEFT, snake_pos.x, snake_pos.y);
 	grid.set(SNAKE, snake_pos.x, snake_pos.y);
 	setBlob();
@@ -95,7 +95,7 @@ function infinity() {
 function update() {
 	frames++;
 	if(keystate[KEY_LEFT] && snake.direction !== RIGHT) { snake.direction = LEFT; }
-	if(keystate[KEY_UP] && snake.direction !==DOWN) { snake.direction = UP; }
+	if(keystate[KEY_UP] && snake.direction !== DOWN) { snake.direction = UP; }
 	if(keystate[KEY_RIGHT] && snake.direction !== LEFT) { snake.direction = RIGHT; }
 	if(keystate[KEY_DOWN] && snake.direction !== UP) { snake.direction = DOWN; }
 	if(frames%10 === 0) {
@@ -114,13 +114,12 @@ function update() {
 			setBlob();
 		}
 		else {
-			var end=snake.remove();
+			var end = snake.remove();
 			grid.set(SPACE, end.x, end.y);
 		}
 		grid.set(SNAKE, xcor, ycor);
 		snake.insert(xcor, ycor);
 	}
-	
 }
 
 function draw() {
