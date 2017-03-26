77f4248afa9eadb5b793531babc314c064cf6986
Tracks <anon.dark97@gmail.com>
2017-03-26 04:31:26 +0200

    Add files via upload

--- php/filelist.php
+++ php/filelist.php
@@ -12,28 +12,30 @@
 		<link rel="stylesheet" type="text/css" href="../css/scroll.css" />
 		<link rel="icon" type="image/png" href="../pics/icon.png" />
 		<?php if(isset($_SESSION['user'])) { include('./theme.php'); } ?>
+		<script language="javascript" type="text/javascript" src="../js/script.js"></script>
 	</head>
 	<body>
 		<form action="./filelist.php" method="get">
 			<?php
-				$page = 'admin.php';
+				$page = 'filelist.php';
 				include('./session.php');
 				
 				if($_SESSION['profile'] != 'ADMIN') { header('location: ../client.php'); }
+				$i = 0;
 				
 				echo('<input type="hidden" name="target" value="'.$_GET['target'].'" />');
 				for($tour = 0; $tour < 2; $tour++) {
 					$data = [$bdd->query('SELECT * FROM donnee'), $bdd->query('SELECT * FROM donnee'), $bdd->query('SELECT * FROM donnee')];
 					
 					switch($tour) {
-						case 0:
-							while($file = $data[0]->fetch()) {
-								if($_GET['target'] == $file[1]) { list_dossiers($file, $dir, $a); }
+						case 0: while($file = $data[0]->fetch()) {
+								if($_GET['target'] == $file[1]) { list_dossiers($file, $dir, $a, $i); }
+								$i++;
 							} break;
 						
-						case 1:
-							while($file = $data[0]->fetch()) {
-								if($_GET['target'] == $file[1]) { list_fichiers($file, $dir, $a); }
+						case 1: while($file = $data[0]->fetch()) {
+								if($_GET['target'] == $file[1]) { list_fichiers($file, $dir, $a, $i); }
+								$i++;
 							} break;
 					}
 				}
